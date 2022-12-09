64a61e36a6ddb439c9799ef7d1cfb8a70b83972d
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-12-08 19:48:03 -0500

    Styled Habit card

---
+++ components/Habit.js
@@ -0,0 +1,129 @@
+import styles from '../styles/Habit.module.css';
+import { useState } from 'react'
+
+export default function Habit(props) {
+
+  const [editFormShowing, setEditFormShowing] = useState(false);
+  const [editButtonText, setEditButtonText] = useState('Edit')
+  const [cardStyle, setCardStyle] = useState('card');
+  const deleteHandler = () => {
+    props.deleteHandler(props.identifier);
+  }
+  
+  const toggleEditForm = () => {
+    console.log('User is toggling the edit form');
+    setEditFormShowing(!editFormShowing);
+    setEditButtonText(editButtonText === 'Edit' ? 'Close' : 'Edit');
+  }
+
+  const editHandler = (event) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault()
+    
+    const editedHabit = event.target.habit.value;
+    console.log("editedHabit: ", editedHabit);
+    props.editHandler(props.identifier, editedHabit);
+  }  
+
+
+  const styleCard = () => {
+    if (cardStyle === "cardClicked") {
+      setCardStyle("card");
+    } else {
+      setCardStyle("cardClicked");
+    }
+  }
+
+  return (
+    <>
+      { props.cardType === "dashboard" ?
+        <a onClick={() => styleCard()} className={cardStyle}>
+          <h1>{props.habit.text}</h1>
+        </a> :
+        <a onClick={() => props.styleCard(props.habit.id)} className={props.habit.habit_card_style}>
+          <h1>{props.habit.habit_emoji}</h1>
+          <h2>{props.habit.habit_name}</h2>
+        </a>
+      }
+      <style jsx>{`
+          .card {
+            margin: 1rem;
+            flex-basis: 40%;
+            padding: 1.5rem;
+            text-align: left;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.3s ease, border-color 0.3s ease;
+          }
+
+          .cardClicked {
+            margin: 1rem;
+            flex-basis: 40%;
+            padding: 1.5rem;
+            text-align: left;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.3s ease, border-color 0.3s ease;
+            background-color: #CBC3E3;
+          }
+
+          .card:hover,
+          .card:focus,
+          .card:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+        `}</style>
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+    </>
+  )
+}
+
+
+
+
+
+{/* {
+        editFormShowing ?
+        (
+          <>
+            <div className={styles.container}>
+              <form onSubmit={editHandler} className={styles.editForm}>
+                <label htmlFor="habit">Edit Habit: </label>
+                <input type="text" id="habit" name="habit" required placeholder={props.habit.text}/>
+                <button type="submit">Submit</button>
+              </form>
+              <div className={styles.buttonContainer}>
+                <button onClick={deleteHandler}>Delete</button>
+                <button onClick={toggleEditForm}>{editButtonText}</button>
+              </div>
+            </div>
+          </>
+        )
+        : (
+          <>
+            <div className={styles.container}>
+              <p className={styles.p}>{props.habit.text}</p>   
+              <div className={styles.buttonContainer}>
+                <button onClick={deleteHandler}>Delete</button>
+                <button onClick={toggleEditForm}>Edit</button>
+              </div>
+            </div>
+          </>
+        )
+      } */}
---
+++ pages/index.js
@@ -0,0 +1,143 @@
+import Head from 'next/head'
+import clientPromise from '../lib/mongodb'
+import { useState } from 'react'
+import Habit from '../components/Habit'
+import uuid from 'react-uuid';
+import styles from '../styles/index.module.css'
+
+export async function getServerSideProps(context) {
+  try {
+    await clientPromise
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
+}
+
+export default function Home({ isConnected }) {
+  
+  const [habits, setHabits] = useState([]);
+
+  const handleSubmit = async (event) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault()
+  
+    // Get data from the form.
+    const data = {
+      habit: event.target.habit.value,
+    }
+  
+    // Send the data to the server in JSON format.
+    const JSONdata = JSON.stringify(data)
+  
+    // API endpoint where we send form data.
+    const endpoint = '/api/form'
+  
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: 'POST',
+      // Tell the server we're sending JSON.
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    }
+  
+    // Send the form data to our forms API on Vercel and get a response.
+    const response = await fetch(endpoint, options)
+  
+    // Get the response data from server as JSON.
+    // If server returns the name submitted, that means the form works.
+    const result = await response.json()
+  
+    const newHabit = {
+      key: uuid(),
+      text: result.data,
+    }
+    setHabits(current => [...current, newHabit]);
+    event.target.habit.value = "";
+  }
+
+  const deleteHandler = (identifier) => {
+    setHabits(habits.filter(habit => habit.key !== identifier));
+  }
+
+  const editHandler = (identifier, editedHabit) => {
+    const habitToEdit = habits.filter(habit => habit.key === identifier);
+    habitToEdit.text = editedHabit;
+    habitToEdit.key = uuid();
+
+    deleteHandler(identifier);
+    setHabits(current => [...current, habitToEdit]);
+  }
+
+  return (
+    <>  
+      <div className={styles.headerContainer}>
+        <h1>Habit Tracker</h1>
+      </div>
+      <div className="container">
+      <Head>
+        <title>Habit Tracker</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input className={styles.input} placeholder="Enter Habit" type="text" id="habit" name="habit" required />
+        <button className={styles.submitButton} type="submit">Submit</button>
+      </form>
+      <div className="grid">
+      {
+        habits.map((habit) => {
+          return <Habit key={habit.key} cardType={"dashboard"} habit={habit} identifier={habit.key} deleteHandler={deleteHandler} editHandler={editHandler}/>
+        })
+      }
+      </div>
+        <style jsx>{`
+          .grid {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-wrap: wrap;
+            max-width: 1000px;
+            margin-top: 3rem;
+          }
+
+          .container {
+            padding: 0 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+          `}</style>
+      </div>
+    </>
+  )
+}
+
+    // The code below is to check if this app is connected to the database
+
+    {/* 
+    <main>
+      {isConnected ? (
+        <h2 className="subtitle">You are connected to MongoDB</h2>
+      ) : (
+        <h2>You are NOT connected to MongoDB.</h2>
+      )}
+    </main> */}
